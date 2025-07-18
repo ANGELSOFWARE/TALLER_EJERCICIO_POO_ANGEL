6d9b16e79602602b746e57e0aa2e4676894190ca
ANGELSOFWARE <angelcastellanos483@gmail.com>
2025-07-17 20:41:38 -0500

    CORRAL.PYTHON.py

--- CORRAL.PYTHON.py
+++ CORRAL.PYTHON.py
@@ -1,16 +1,15 @@
 class Corral:
-    def __init__(self, id_corral, tipo, capacidad_maxima):
-        self.id_corral = id_corral
-        self.tipo = tipo  
-        self.capacidad_maxima = capacidad_maxima
-        self.animales = []  
+    def __init__(self, idCorral, capacidad, estado):
+        self.idCorral = idCorral
+        self.capacidad = capacidad
+        self.estado = estado
+        self.animales = []
 
-    def asignar_animal(self, animal):
-        if len(self.animales) < self.capacidad_maxima:
-            self.animales.append(animal)
-            print(f"{animal._nombre} ha sido asignado al corral {self.id_corral} ({self.tipo}).")
-        else:
-            print(f"No hay espacio en el corral {self.id_corral} para {animal._nombre}.")
-    
     def limpiar(self):
-        print(f"El corral {self.id_corral} ha sido limpiado.")
+        print(f"Corral {self.idCorral} limpiado")
+
+    def asignarAnimal(self, animal):
+        self.animales.append(animal)
+
+    def verificarEstado(self):
+        print(f"Estado del corral {self.idCorral}: {self.estado}")
